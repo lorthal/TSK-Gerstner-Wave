5dd5ef7da13f4726107a86145d89947a387d7f7a
Bartosz Kluchciński <bkluchcinski@gmail.com>
2017-12-18 19:33:29 +0100

    added changing direction with arrow

--- Assets/Modifier.cs
+++ Assets/Modifier.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Modifier : MonoBehaviour {
+public class Modifier : MonoBehaviour
+{
 
     public Material mat;
+
+    public GameObject arrow;
+
     public GameObject speedSlider;
     public Text txt;
-	// Update is called once per frame
-	void Update () {
-        mat.SetFloat("_Speed", speedSlider.GetComponent<Slider>().value);
-        txt.text = speedSlider.GetComponent<Slider>().value.ToString() + " m/s";
 
+    public Vector3 dir;
+
+    private Vector2 pass;
+    // Update is called once per frame
+    void Update()
+    {
+        dir = Vector3.Normalize(arrow.transform.rotation * Vector3.right);
+
+        pass = new Vector2(dir.x, dir.z);
+
+        mat.SetFloat("_Speed", speedSlider.GetComponent<Slider>().value);
+        mat.SetVector("_Direction", pass);
+        txt.text = speedSlider.GetComponent<Slider>().value.ToString() + " m/s";
     }
 }
--- Assets/WindDirection.cs
+++ Assets/WindDirection.cs
@@ -8,18 +8,12 @@ public class WindDirection : MonoBehaviour
     float speed = 80.0f;
     Transform target;
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            transform.Rotate(new Vector3(-Input.GetAxis("Mouse X"), 0, 0) * Time.deltaTime * speed);
+            transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speed);
         }
     }
 }
